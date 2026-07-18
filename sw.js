15a98e116db7714abf8255b639cacc2250db1370
SchwarzGold-Detailing <ghettokind19@live.de>
2026-07-18 21:39:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+/* Service Worker: macht die App offline nutzbar */
+const CACHE = "solobuch-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+  );
+});
